3f78f31ffbe9d5d356fc359cfd5489d4053993ad
DJ <devrichill@gmail.com>
2026-09-17 14:50:08 -0400

    Add project files.

---
+++ Assignment_4.3/ElectircBill.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_4._3
+{
+    delegate void UnitAmount(float a);
+    internal class ElectircBill
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public float UnitConsumed { get; set; }
+
+        private string _textCharged;
+        private float _amountCharge;
+        private float _surChargeAmount;
+        private float _netAmountPaid;
+
+        public ElectircBill(int id, string name, float units)
+        {
+            CustomerId = id;
+            CustomerName = name;
+            UnitConsumed = units;
+        }
+
+        public bool IsOverAmount(float cost)
+        {
+            return cost > 400;
+        }
+
+        public float AddSurCharge(float cost)
+        {
+            return 0.15f * cost;
+        }
+
+        public float CheckSurCharge(float cost)
+        {
+            if (IsOverAmount(cost)) return AddSurCharge(cost);
+            return 0;
+        }
+
+        public void SmallUnits(float units)
+        {
+            _textCharged = "@$ 1.20 per unit";
+            _amountCharge = 1.20f * units;
+            _surChargeAmount = CheckSurCharge(_amountCharge);
+            _netAmountPaid = _amountCharge + _surChargeAmount;
+        }
+
+        public void MediumUnits(float units)
+        {
+            _textCharged = "@$ 1.50 per unit";
+            _amountCharge = 1.50f * units;
+            _surChargeAmount = CheckSurCharge(_amountCharge);
+            _netAmountPaid = _amountCharge + _surChargeAmount;
+        }
+
+        public void BigUnits(float units)
+        {
+            _textCharged = "@$ 1.80 per unit";
+            _amountCharge = 1.80f * units;
+            _surChargeAmount = CheckSurCharge(_amountCharge);
+            _netAmountPaid = _amountCharge + _surChargeAmount;
+        }
+
+        public void MaxUnits(float units)
+        {
+            _textCharged = "@$ 2.00 per unit";
+            _amountCharge = 2.00f * units;
+            _surChargeAmount = CheckSurCharge(_amountCharge);
+            _netAmountPaid = _amountCharge + _surChargeAmount;
+        }
+
+        public void CalculateUnits(float units, UnitAmount amount)
+        {
+            amount(units);
+        }
+
+        public void StartBilling()
+        {
+            if (UnitConsumed > 0 && UnitConsumed <= 199)
+            {
+                CalculateUnits(UnitConsumed, SmallUnits);
+            }
+            else if (UnitConsumed >= 200 && UnitConsumed < 400)
+            {
+                CalculateUnits(UnitConsumed, MediumUnits);
+            }
+            else if (UnitConsumed >= 400 && UnitConsumed < 600)
+            {
+                CalculateUnits(UnitConsumed, BigUnits);
+            }
+            else if(UnitConsumed >= 600)
+            {
+                CalculateUnits(UnitConsumed, MaxUnits);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"Customer IDNO: {CustomerId}\n" +
+                $"Customer Name: {CustomerName}\n" +
+                $"Unit Consumed: {UnitConsumed:F2}\n" +
+                $"Amount Charges {_textCharged}: {_amountCharge:F2}\n" +
+                $"Surcharge Amount: {_surChargeAmount:F2}\n" +
+                $"Net Amount Paid By the Customer: {_netAmountPaid:F2}";
+        }
+
+    }
+}
---
+++ Assignment_4.3/Program.cs
@@ -0,0 +1,134 @@
+﻿namespace Assignment_4._3
+{
+    internal class Program
+    {
+        #region Electric Bill Method
+        static void CheckElectricBill()
+        {
+            int id;
+            string name;
+            float units;
+
+
+            Console.WriteLine("Welcome to your electric company! Enter your information to check your bill!\n");
+
+            Console.Write("Enter Your Customer Id: ");
+            id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Your Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter the Amount of Units You Used: ");
+            units = float.Parse(Console.ReadLine());
+
+            ElectircBill bill = new ElectircBill(id, name, units);
+            bill.StartBilling();
+            Console.WriteLine("\n" + bill.ToString());
+
+        }
+        #endregion
+
+        #region Frequency Check Method
+        static int[] AssignArray(int size)
+        {
+            int[] array = new int[size];
+
+            Console.WriteLine($"Input {size} elemets in the array:");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool IsVal = false;
+                int val = 0;
+                while (!IsVal)
+                {
+                    Console.Write($"element - {i}: ");
+                    IsVal = int.TryParse(Console.ReadLine(), out val);
+                }
+
+                array[i] = val;
+            }
+
+            return array;
+        }
+
+        static Dictionary<int, int> Frequency(int[] array)
+        {
+            Dictionary<int, int> temp = new Dictionary<int, int>();
+
+            foreach (int val in array)
+            {
+                if (!temp.ContainsKey(val))
+                {
+                    temp.Add(val, 1);
+                }
+                else
+                {
+                    temp[val]++;
+                }
+
+
+            }
+
+            return temp;
+        }
+
+        static void PrintResults(Dictionary<int, int> temp)
+        {
+            foreach (int x in temp.Keys)
+            {
+                Console.WriteLine($"{x} occurs {temp[x]} times");
+            }
+        }
+
+        static void ArrayFrequency()
+        {
+            Console.Write("\n\nInput the number of the elements to be stored in the array: ");
+            int assignSize = int.Parse(Console.ReadLine());
+            int[] array = AssignArray(assignSize);
+            Dictionary<int, int> FrequencyDictionary = Frequency(array);
+            Console.WriteLine("Frequency of all elements of array:");
+            PrintResults(FrequencyDictionary);
+
+        } 
+        #endregion
+
+        static int UniqueElements(int[] array)
+        {
+            Dictionary<int, int> temp = new Dictionary<int, int>();
+
+            if (array.Length == 0) return 0;
+
+            foreach(int val in array)
+            {
+                if (!temp.ContainsKey(val)) temp.Add(val, 1);
+                else temp[val]++;
+            }
+            
+
+            int total = 0;
+
+            foreach (int val in temp.Keys)
+            {
+                total = temp[val] == 1 ? val : total;
+            }
+
+            return total;
+            
+        }
+
+        static void UniqueElements()
+        {
+            Console.Write("\n\nInput the number of the elements to be stored in the array: ");
+            int size = int.Parse(Console.ReadLine());
+            int[] array = AssignArray(size);
+            Console.WriteLine($"The unique elements found in the array are: {UniqueElements(array)}");
+        }
+
+        static void Main(string[] args)
+        {
+            CheckElectricBill();
+            ArrayFrequency();
+            UniqueElements();
+        }
+    }
+}
